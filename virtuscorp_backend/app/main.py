59c9e25f13c231fb653e49b88c00da8549cfae44
parkjihn <92673192+parkjihn@users.noreply.github.com>
2025-03-30 12:24:02 +0000

    database7

--- virtuscorp_backend/app/main.py
+++ virtuscorp_backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from app.middleware.cors import add_cors_middleware
-from app.api.routes import auth 
+from app.api.routes import auth
+from tortoise.contrib.fastapi import register_tortoise
+from app.db.database import TORTOISE_ORM
 
 app = FastAPI()
 
 add_cors_middleware(app)
 
 app.include_router(auth.router, prefix="/auth")
+
+register_tortoise(
+    app,
+    config=TORTOISE_ORM,
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 @app.get("/")
 def read_root():
